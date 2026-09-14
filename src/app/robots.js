770441a4d7def57dc73ef7c1471ee3d8653a55a2
zannat02 <shawon4166@gmail.com>
2026-09-14 16:47:37 +0600

    Add sitemap and robots for SEO

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://zannatul-ferdous-shawon.vercel.app/sitemap.xml",
+  };
+}
